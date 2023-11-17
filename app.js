d3f65ecabd0ee76d6a1e88f1794dbab70861f8f2
Nazmus Sakib Apurba <nazmussakibapurbo@gmail.com>
2023-11-17 20:27:17 +0600

    1st

---
+++ app.js
@@ -0,0 +1,112 @@
+var web3;
+
+const address="0xD1c20A8CA57e88756Bd61d71e6659B7045b935Bb";
+
+async function connect() {
+    try {
+        await window.currentProvider.enable();
+        web3 = new Web3(window.web3.currentProvider);
+    } catch (error) {
+        console.error(error);
+        document.getElementById("status").innerHTML = "Please connect MetaMask";
+    }
+}
+
+
+if (typeof web3 === "undefined") {
+    document.getElementById("status").innerHTML = "Please connect MetaMask";
+} else {
+    web3 = new Web3(window.web3.currentProvider);
+}
+
+
+const abi=[
+	{
+		"inputs": [
+			{
+				"internalType": "int256",
+				"name": "amt",
+				"type": "int256"
+			}
+		],
+		"name": "deposit",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "int256",
+				"name": "amt",
+				"type": "int256"
+			}
+		],
+		"name": "withdraw",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getbalance",
+		"outputs": [
+			{
+				"internalType": "int256",
+				"name": "",
+				"type": "int256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+const contract=new web3.eth.Contract(abi,address);
+
+function deposit(){
+
+    var input= document.getElementById("amount").value;
+    web3.eth.getAccounts().then(function(account){
+    return contract.methods.deposit(input).send({from:account[0]});
+    }).then(function(tmp){
+        $("#amount").val("");
+        show_bal();
+    }).catch(function(tmp){
+
+        alert(tmp);
+    })
+
+
+}
+
+
+function withdraw(){
+
+    var input= document.getElementById("amount").value;
+    web3.eth.getAccounts().then(function(account){
+    return contract.methods.withdraw(input).send({from:account[0]});
+    }).then(function(tmp){
+        $("#amount").val("");
+        show_bal();
+    }).catch(function(tmp){
+
+        alert(tmp);
+    })
+
+}
+
+
+
+function show_bal(){
+
+    contract.methods.getbalance().call().then(function(balance){
+
+            $("#balance").html(balance);
+    })
+}
